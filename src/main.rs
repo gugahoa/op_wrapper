5f248e2936a92dbf257d046c7e9bfd09c17a53ac
Gustavo Aguiar <gugahoa@gmail.com>
2018-05-24 19:16:28 -0300

    Hello World cli application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![feature(proc_macro)]
+extern crate clap;
+extern crate thunder;
+use thunder::thunderclap;
+
+struct PassApp;
+
+#[thunderclap]
+impl PassApp {
+    fn hello(name: &str) {
+        println!("Hello, {}!", name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    PassApp::start();
 }
