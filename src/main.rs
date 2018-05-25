76a5d044aaf29607e8c6396d98a1ac69441155ad
Gustavo Aguiar <gugahoa@gmail.com>
2018-05-25 00:39:33 -0300

    Add 1pass get item example

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,28 @@ extern crate clap;
 extern crate thunder;
 use thunder::thunderclap;
 
+use std::process::Command;
+
 struct PassApp;
 
 #[thunderclap]
 impl PassApp {
     fn hello(name: &str) {
         println!("Hello, {}!", name);
+    }
+    fn get(item: &str, designation: Option<&str>) {
+        println!("{:?} {:?}", item, designation);
+        let mut command = Command::new("op");
+        let mut_command = command
+            .arg("get")
+            .arg("item")
+            .arg(item);
+
+        if let Some(_designation) = designation {
+        }
+
+        let output = mut_command.output().expect("Failed to execute 'op'");
+        println!("{:?} {:?}", output, mut_command);
     }
 }
 
