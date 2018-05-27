7b896ddf4e68785c62a508843c30ea6331dfbfa8
Gustavo Aguiar <gugahoa@gmail.com>
2018-05-27 15:32:47 -0300

    Add validation code

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,56 @@ extern crate isatty;
 
 use spinner::SpinnerBuilder;
 use thunder::thunderclap;
-use std::process::Command;
+use std::process::{Command, Stdio, exit};
 use isatty::stdout_isatty;
+
+fn validate_requirements(namespace: Option<String>) {
+    let code = Command::new("op")
+        .stdout(Stdio::null())
+        .status()
+        .expect("Failed to execute 'op'")
+        .code()
+        .expect("Received no exit status from 'op'");
+    if code == 127 {
+        eprintln!("'op' not found in $PATH");
+        exit(1);
+    }
+
+    let namespace = match namespace {
+        None => {
+            eprintln!("Warning: Namespace not specified, using: my");
+            "my".into()
+        },
+        Some(namespace) => namespace
+    };
+
+    let session_key = format!("OP_SESSION_{}", namespace);
+    if std::env::var(&session_key).is_err() {
+        eprintln!("Warning: 1Password not set for '{}'", namespace);
+        let output = Command::new("op")
+            .arg("signin")
+            .arg(namespace)
+            .arg("--output=raw")
+            .stdin(Stdio::inherit())
+            .output()
+            .expect("Failed to execute 'op'");
+
+        if !output.status.success() {
+            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+            exit(output.status.code().unwrap());
+        }
+
+        let session_value = String::from_utf8_lossy(&output.stdout).to_string();
+        std::env::set_var(session_key, session_value.trim());
+    }
+}
 
 struct PassApp;
 
-#[thunderclap]
+#[thunderclap(namespace: Option<String>: "1Password namespace to use", vault: Option<String>: "Specify Vault to look for items")]
 impl PassApp {
-    fn hello(name: &str) {
-        println!("Hello, {}!", name);
-    }
     fn get(item: &str, designation: Option<&str>) {
+        validate_requirements(Self::namespace());
         let sp = if stdout_isatty() {
             Some(SpinnerBuilder::new("Waiting on op-cli...".into()).start())
         } else { None };
